934e54beb57032aae2678b1f3b5fd0a7f3cc2b40
morjina <fmorjina111@gmail.com>
2025-03-06 10:22:56 +0600

    Setup Menu Position

--- functions.php
+++ functions.php
@@ -23,8 +23,12 @@
   wp_enqueue_script('main',get_template_directory(  ) .'/js/main.js',array('jquery'),'1.0.0',true);
 }
 add_action('wp_enqueue_scripts','ali_css_js_file_calling');
-
-
+//goole font calling
+function ali_google_font_calling(){
+  wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet',false);
+  wp_enqueue_style('googlefont');
+}
+add_action('wp_enqueue_scripts','ali_google_font_calling');
 // Theme function for customizer
 function ali_customizar_register($wp_customize) {
   // Add section for header area
@@ -35,7 +39,7 @@ function ali_customizar_register($wp_customize) {
 
   // Add setting for logo upload
   $wp_customize->add_setting('ali_logo', array(
-      'default' => get_template_directory_uri() . '/imgs/mylogo.png', // Corrected path function
+      'default' => get_template_directory_uri() . '/imgs/mylogo.png', 
       'transport' => 'refresh',
   ));
 
@@ -45,11 +49,40 @@ function ali_customizar_register($wp_customize) {
       'section'  => 'ali_Header_area',
       'settings' => 'ali_logo',
   )));
+  //manu position
+  $wp_customize->add_section('ali_menu_option', array(
+    'title' =>__('Menu position option','twentytwentyfour'),
+    'description' => __('If you want to update your menu position, use this option.', 'alihossain'), // Corrected description
+  ));
+
+  // Add setting for menu position
+  $wp_customize->add_setting('ali_menu_position', array(
+    'default' => 'right_ menu',
+
+  ));
+
+  $wp_customize->add_control('ali_menu_position',array(
+   'label' => 'menu position',
+   'description' => 'select your menu position',
+   'settings' => 'ali_menu_position',
+   'section'  => 'ali_menu_option',
+   'type' => 'radio',
+   'choices' => array(
+     'left_menu'=> 'left Menu',
+     'right_menu' => 'right Menu',
+     'center_menu' => 'center Menu'
+   )
+
+  ));
+ 
 }
 
 // Correct hook name
 add_action('customize_register', 'ali_customizar_register');
 
+
+// wp Menu register
+register_nav_menu('main_menu', __('Main Menu','twentytwentyfour'));
 
 ?>
 
